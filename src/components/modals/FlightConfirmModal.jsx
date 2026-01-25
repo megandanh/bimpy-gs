9d85bb9e62500eb8ec891eacd06251b3e063b382
megandanh <mbndanh2k4@gmail.com>
2026-01-24 20:45:17 -0500

    test identity

---
+++ src/components/modals/FlightConfirmModal.jsx
@@ -0,0 +1,40 @@
+import "./flightConfirm.css";
+
+export default function StartFlightModal({
+  isOpen,
+  recordOn,
+  setRecordEnabled,
+  onConfirm,
+  onCancel,
+}) {
+  if (!isOpen) return null;
+
+  return (
+    <div className="modal-backdrop" onClick={onCancel}>
+      <div className="modal" onClick={(e) => e.stopPropagation()}>
+        <h2 className="modal-title">Would you like to record this flight?</h2>
+
+        <div className="record-row">
+          <div className="record-labels">
+            <div className="record-label">Recording: </div>
+            <div className="record-status">{recordOn ? "ON" : "OFF"}</div>
+          </div>
+
+          <label className="switch">
+            <input
+              type="checkbox"
+              checked={recordOn}
+              onChange={(e) => setRecordEnabled(e.target.checked)}
+            />
+            <span className="slider" />
+          </label>
+        </div>
+
+        <div className="modal-actions">
+          <button className="btn-secondary" onClick={onCancel}> Cancel</button>
+          <button className="btn-primary" onClick={onConfirm}>Start Flight</button>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/ConsolePage/ConsolePage.jsx
+++ src/pages/ConsolePage/ConsolePage.jsx
@@ -1,19 +1,58 @@
 import "./console.css";
+import { useState } from "react";
+import FlightConfirmModal from "../../components/modals/FlightConfirmModal";
+import BimpyLogo from "../../assets/BimpyLogo.png";
 import FlightMessages from "../../components/panels/FlightMessagesPanel";
 import MapPanel from "../../components/panels/MapPanel";
 
-export default function ConsolePage() {
-  return (
-    <div className="console">
-      <div className="col col-left">
-        <FlightMessages />
-      </div>
 
-      <div className="col col-mid">
-        <MapPanel />
-      </div>
-    
-      <div className="col col-right">{/* later */}</div>
-    </div>
+export default function ConsolePage() {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [isFlightRunning, setIsFlightRunning] = useState(false);
+    const [isRecordingOn, setIsRecordingOn] = useState(false);
+
+    const onClickStartFlight = () => {
+        setIsModalOpen(true);
+    };
+
+    const onConfirmStartFlight = () => {
+        setIsModalOpen(false);
+        setIsFlightRunning(true);
+    }
+
+    const onCancelFlight = () => {
+        setIsModalOpen(false);
+        setIsRecordingOn(false); 
+    }
+    return (
+        <div className="console">
+            <div className="console-header">
+                <img src= {BimpyLogo} alt="Bimpy Logo" id="bimpyLogoConsole"/>
+                <div className="console-buttons">
+                    <button id="startFlightBtn" onClick={onClickStartFlight}>START FLIGHT</button>
+                    <button id="flightAnalysisBtn">ANALYZE</button>
+                </div>
+            </div>
+
+            <FlightConfirmModal
+                isOpen={isModalOpen}
+                recordOn={isRecordingOn}
+                setRecordEnabled={setIsRecordingOn}
+                onConfirm={onConfirmStartFlight}
+                onCancel={onCancelFlight}
+            />
+
+            <div className="console-body">
+                <div className="col col-left">
+                    <FlightMessages />
+                </div>
+
+                <div className="col col-mid">
+                    <MapPanel />
+                </div>
+                
+                <div className="col col-right">{/* later */}</div>
+            </div>
+        </div>
   );
 }
